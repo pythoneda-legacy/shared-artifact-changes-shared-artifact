3501dbe8676c8364ba7a3a73ca9dfdad94506e18
rydnr <github@acm-sl.org>
2023-08-16 18:58:31 +0200

    Updated dependencies

--- shared/flake.nix
+++ shared/flake.nix
@@ -23,13 +23,13 @@
     nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
     pythoneda-shared-pythoneda-banner = {
-      url = "github:pythoneda-shared-pythoneda/banner/0.0.1a7";
+      url = "github:pythoneda-shared-pythoneda/banner/0.0.1a10";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
     };
     pythoneda-shared-pythoneda-domain = {
       url =
-        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a30?dir=domain";
+        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a31?dir=domain";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
@@ -42,8 +42,8 @@
       let
         org = "pythoneda-shared-artifact-changes";
         repo = "shared";
-        version = "0.0.1a4";
-        sha256 = "sha256-XG94m7OVTlUcd4F9Imh3vLeiw5hR4ml6GTMgEzjQAag=";
+        version = "0.0.1a5";
+        sha256 = "sha256-xzzelewAm1BAsAxlHQWQcP7FZ5nCeCbiDeejEw6QpTs=";
         pname = "${org}-${repo}";
         pkgs = import nixos { inherit system; };
         description =
